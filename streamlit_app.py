282fb8d1f9e16765d030dbfa59c0ae1c1bea1cfa
SatyaSurisetti <55866936+SatyaSurisetti@users.noreply.github.com>
2023-07-07 14:36:47 +0800

    added breakfast menu and 3 items

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,6 @@
 import streamlit
 streamlit.title (" This is my first python program on GitHub")
-streamlit.title (" Breakfast Menu")
-streamlit.title ("Omega 3 and black berry oatmeal" )
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
