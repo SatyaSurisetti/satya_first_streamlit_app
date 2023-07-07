6d82acd3b6ecb63db6b2678b0772d5ff6217443a
SatyaSurisetti <55866936+SatyaSurisetti@users.noreply.github.com>
2023-07-07 14:40:21 +0800

    modified text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit
 streamlit.title (" This is my first python program on GitHub")
-streamlit.header('Breakfast Menu')
+streamlit.header('Breakfast favourites')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.text('avacado toast')
