4364915cfa5924a72ca743d603e8277b4a872f54
SatyaSurisetti <55866936+SatyaSurisetti@users.noreply.github.com>
2023-07-07 14:35:16 +0800

    added couple of display messages to existing content

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,4 @@
 import streamlit
 streamlit.title (" This is my first python program on GitHub")
-streamlit.title (" Breakfast Menu" /n "Omega 3 and black berry oatmeal" )
+streamlit.title (" Breakfast Menu")
+streamlit.title ("Omega 3 and black berry oatmeal" )
