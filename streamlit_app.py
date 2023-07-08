cfe27fce9bfa31ebbff70eb0f4b75e23c3d5afdd
SatyaSurisetti <55866936+SatyaSurisetti@users.noreply.github.com>
2023-07-08 17:40:13 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,7 +18,12 @@ streamlit.multiselect("pick some fruits:", list(my_fruit_list.index), ['Avocado'
 #fruityvice_response = requests.get("Hai Satya what are you doing?")
 
 streamlit.header("Fruityvice Fruit Advice!")
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "Kiwi")
+
+# Add a Text Entry Box and Send the Input to Fruityvice as Part of the API Call
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "fruit_choice")
 #streamlit.text(fruityvice_response.json())
 # Below function calls a pandas function to show the json file in a normalized form. 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
