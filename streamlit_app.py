80ab8ec9c514fcd7894968bf747a0f5a9a2dfee1
SatyaSurisetti <55866936+SatyaSurisetti@users.noreply.github.com>
2023-07-08 17:25:59 +0800

    declared function and calling it in next lines

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,6 +14,6 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 streamlit.multiselect("pick some fruits:", list(my_fruit_list.index), ['Avocado','Strawberries'])
 
 # new section to display fruity vice api response
-
+streamlit.header("Fruityvice Fruit Advice!")
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response)
