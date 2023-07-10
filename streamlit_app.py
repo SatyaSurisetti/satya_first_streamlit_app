e60683ada6747bd5e0a885d0a4f2b0b061a188f1
SatyaSurisetti <55866936+SatyaSurisetti@users.noreply.github.com>
2023-07-10 15:14:45 +0800

    Added addtional function for user inputs

--- streamlit_app.py
+++ streamlit_app.py
@@ -51,9 +51,13 @@ if streamlit.button('Get Fruit Load List'):
     my_data_rows = get_fruit_load_list()
     streamlit.dataframe(my_data_rows)
 
-# Add a Text Entry Box and Send the Input to Fruityvice as Part of the API Call
+# Allow the end user to add a fruit to the list
+def insert_row_snowflake(new_fruit):
+    with my_cnx.cursor() as my_cur:
+        my_cur.execute("insert into PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST values ('from streamlit')");
+        return "Thanks for adding" + new_fruit
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
-streamlit.write('Thanks for adding', add_my_fruit)
-
-# This will not work correctly but just go with in for now
-streamlit.write("insert into PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST values ('from streamlit')");
+if streamlit.button('Add a Fruit to the List'):
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    back_from_function = insert_row_snowflake(add_my_fruit)
+    streamlit.text(back_from_function)
