d31b8f8246fb85ea35dfaa187901a34f089485b3
SatyaSurisetti <55866936+SatyaSurisetti@users.noreply.github.com>
2023-07-10 13:50:54 +0800

    added addtional text field to add another fruit

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,6 +24,7 @@ streamlit.header("Fruityvice Fruit Advice!")
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
 
+
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 #streamlit.text(fruityvice_response.json())
 # Below function calls a pandas function to show the json file in a normalized form. 
@@ -38,3 +39,7 @@ my_cur.execute("SELECT * from FRUIT_LOAD_LIST")
 my_data_rows = my_cur.fetchall()
 streamlit.header("The Fruit Load list contains")
 streamlit.dataframe(my_data_rows)
+
+# Add a Text Entry Box and Send the Input to Fruityvice as Part of the API Call
+add_my_fruit = streamlit.text_input('What fruit would you like to add?')
+streamlit.write('The user entered ', add_my_fruit)
