20b1f621ba8aeb69d5a977e62ab5f5ce78ea1721
SatyaSurisetti <55866936+SatyaSurisetti@users.noreply.github.com>
2023-07-10 14:55:33 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,7 +20,7 @@ streamlit.multiselect("pick some fruits:", list(my_fruit_list.index), ['Avocado'
 def get_fruityvice_data(this_fruit_choice):
     fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
     fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-  return fruityvice_normalized
+    return fruityvice_normalized
 
 # new section to display fruity vice api response
 streamlit.header("Fruityvice Fruit Advice!")
