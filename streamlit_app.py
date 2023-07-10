26e80c8fb9636f5183d4b2ff7002bbc4dbdc2d15
SatyaSurisetti <55866936+SatyaSurisetti@users.noreply.github.com>
2023-07-10 14:35:00 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 #import pandas
 #import requests
 import snowflake.connector
-from URrllib.error import URLError
+from urllib.error import URLError
 
 # dont run anything past here while we troubleshoot
 streamlit.stop()
